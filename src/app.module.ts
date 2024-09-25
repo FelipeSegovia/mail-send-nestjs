5abe8866bffbaca639daf9acb9ea4f6ca88ab157
fsegovia <felipe.segovia.rod@gmail.com>
2024-09-25 14:05:20 -0300

    se agrega un welcome a la raiz

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MailModule } from './mail/mail.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
     MailModule,
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
